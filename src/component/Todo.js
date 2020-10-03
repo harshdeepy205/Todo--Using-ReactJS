717e3950dc435d99bfe63678bfa5ad614968f60f
harshdeepy205 <harshdeep205@outlook.com>
2020-10-03 14:42:34 +0530

    Initial Commit

---
+++ src/component/Todo.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react'
+import TodoForm from './TodoForm';
+import { RiCloseCircleLine, RiEdit2Line } from 'react-icons/ri'
+// import { RiEdit2Line } from 'react-icon/ri';
+// import { Edit } from '@material-ui/icons';
+
+function Todo({ todos, completeTodo, removeTodo, updateTodo }) {
+
+    const [edit, setEdit] = useState({
+        id: null,
+        value: ''
+    })
+
+    const submitUpdate = value => {
+        updateTodo(edit.id, value)
+        setEdit({
+            id: null,
+            value: ''
+        })
+    }
+    if (edit.id) {
+        return <TodoForm edit={edit} onSubmit={submitUpdate} />
+    }
+
+
+    return todos.map((todo, index) => (
+        <div className={todo.isComplete ? 'todo-row complete' : 'todo-row'} key={index}>
+
+            <div key={todo.id} onClick={() => completeTodo(todo.id)}>
+                {todo.text}
+            </div>
+            <div className="icons">
+                <RiCloseCircleLine onClick={() => removeTodo(todo.id)} className='delete-icon' />
+                <RiEdit2Line onClick={() => setEdit({ id: todo.id, value: todo.text })} className='edit-icon' />
+                {/* <TiEdit /> */}
+
+                {/* <Edit /> */}
+            </div>
+
+
+        </div>
+    ))
+}
+export default Todo
---
+++ src/component/TodoForm.js
@@ -0,0 +1,31 @@
+import React, { useState, useRef, useEffect } from 'react'
+
+function TodoForm(props) {
+    const [input, setInput] = useState('');
+    const inputRef = useRef(null);
+
+    useEffect(() => {
+        inputRef.current.focus()
+    })
+    const handleChange = e => {
+        setInput(e.target.value)
+    }
+
+    const handelSubmit = e => {
+        e.preventDefault();
+
+        props.onSubmit({
+            id: Math.floor(Math.random() * 10000),
+            text: input
+        })
+        setInput('')
+    }
+    return (
+        <form className='todo-form' onSubmit={handelSubmit}>
+            <input type='text' placeholder='Add Todo' value={input} name='text' className='todo-input' onChange={handleChange} ref={inputRef} />
+            <button className='todo-button'>Add Todo</button>
+        </form>
+    )
+}
+
+export default TodoForm;
